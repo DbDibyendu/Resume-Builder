f986bc6309e89004bb5c97bb1b7ef8ab89e5ed35
Dibyendu <dbdibyendu5@gmail.com>
2021-05-17 03:08:20 +0530

    edited

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -83,10 +83,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-
-db_test = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_test)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
